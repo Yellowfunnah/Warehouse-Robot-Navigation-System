f9c7cd4651c6287fd13b51a372166b44f3ef3478
Yellowfunnah <afnansaleh889@gmail.com>
2026-06-02 00:47:34 +0800

    test

--- main.cpp
+++ main.cpp
@@ -3,7 +3,64 @@
 
 using namespace std;
 
+// Function Prototype
+void robotNavigationMenu();
+
 int main()
+{
+    int choice;
+
+    do
+    {
+        cout << "\n=========================================" << endl;
+        cout << " WAREHOUSE ROBOT NAVIGATION SYSTEM" << endl;
+        cout << "=========================================" << endl;
+        cout << "1. Order Management" << endl;
+        cout << "2. Robot Assignment" << endl;
+        cout << "3. Robot Navigation" << endl;
+        cout << "4. Item Management" << endl;
+        cout << "5. Warehouse Layout" << endl;
+        cout << "0. Exit" << endl;
+        cout << "=========================================" << endl;
+        cout << "Enter Choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "\nOrder Management Module Not Implemented Yet." << endl;
+            break;
+
+        case 2:
+            cout << "\nRobot Assignment Module Not Implemented Yet." << endl;
+            break;
+
+        case 3:
+            robotNavigationMenu();
+            break;
+
+        case 4:
+            cout << "\nItem Management Module Not Implemented Yet." << endl;
+            break;
+
+        case 5:
+            cout << "\nWarehouse Layout Module Not Implemented Yet." << endl;
+            break;
+
+        case 0:
+            cout << "\nExiting System..." << endl;
+            break;
+
+        default:
+            cout << "\nInvalid Choice!" << endl;
+        }
+
+    } while (choice != 0);
+
+    return 0;
+}
+
+void robotNavigationMenu()
 {
     RobotNavigation robot;
 
@@ -20,7 +77,7 @@ int main()
         cout << "4. Display Path" << endl;
         cout << "5. Return To Start" << endl;
         cout << "6. Simulate Obstacle" << endl;
-        cout << "0. Exit" << endl;
+        cout << "0. Back to Main Menu" << endl;
         cout << "=================================" << endl;
         cout << "Enter Choice: ";
         cin >> choice;
@@ -52,14 +109,12 @@ int main()
             break;
 
         case 0:
-            cout << "Exiting Program..." << endl;
+            cout << "\nReturning to Main Menu..." << endl;
             break;
 
         default:
-            cout << "Invalid Choice!" << endl;
+            cout << "\nInvalid Choice!" << endl;
         }
 
     } while (choice != 0);
-
-    return 0;
 }
